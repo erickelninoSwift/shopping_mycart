2c3fa377286dd3b7aeec0008e0a48265b089d4fd
Erick El nino <erick.tshimbombo@yahoo.com>
2023-09-01 03:04:10 +0200

    shopping cart functionalities

---
+++ src/Context.jsx
@@ -0,0 +1,83 @@
+import { useState, createContext, useContext, useReducer } from "react";
+import cartItems from "./data";
+import {
+  CLEAR_CART,
+  REMOVE,
+  LOADING,
+  INCREASE,
+  DECREASE,
+  DISPLAY_ITEM,
+} from "./Action";
+
+const AppContext = createContext();
+
+const defaultState = {
+  items: cartItems,
+  loading: false,
+  cart: new Map(
+    cartItems.map((items) => {
+      return [items.id, items];
+    })
+  ),
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case CLEAR_CART:
+      return { ...state, cart: new Map() };
+      break;
+
+    case REMOVE:
+      const newCart = new Map(state.cart);
+      newCart.delete(action.payload.id);
+      return { ...state, cart: newCart };
+      break;
+    case INCREASE:
+      const mycart = new Map(state.cart);
+      const item = mycart.get(action.payload.id).amount++;
+      return { ...state, cart: mycart };
+      break;
+    case DECREASE:
+      let mycart1 = new Map(state.cart);
+      let item1 =
+        mycart1.get(action.payload.id).amount > 0
+          ? mycart1.get(action.payload.id).amount--
+          : 0;
+      return { ...state, cart: mycart1 };
+      break;
+  }
+
+  throw new Error("There was an issue with your reducer");
+};
+
+const AppContextApplication = ({ children }) => {
+  const [state, dispach] = useReducer(reducer, defaultState);
+
+  const clearCart = () => {
+    dispach({ type: CLEAR_CART });
+  };
+
+  const removeItem = (id) => {
+    dispach({ type: REMOVE, payload: { id } });
+  };
+
+  const increaseItem = (id) => {
+    dispach({ type: INCREASE, payload: { id } });
+  };
+
+  const decrease = (id) => {
+    dispach({ type: DECREASE, payload: { id } });
+  };
+  return (
+    <AppContext.Provider
+      value={{ ...state, clearCart, removeItem, increaseItem, decrease }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useAppContextApplication = () => {
+  return useContext(AppContext);
+};
+export default AppContextApplication;
